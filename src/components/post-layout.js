79e76f94548374d4a4a814e733adc08585416605
Karen Wong <karen@fastforward.sh>
2023-02-28 14:29:15 -0500

    Added images, content, layout features

--- src/components/post-layout.js
+++ src/components/post-layout.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Link } from 'gatsby';
 import Layout from './layout.js';
 
-export default function PostLayout({ children, pageContent }) {x
+export default function PostLayout({ children, pageContent }) {
   const { title, description } = pageContent.frontmatter;
   return (
     <Layout title={title} description={description}>
